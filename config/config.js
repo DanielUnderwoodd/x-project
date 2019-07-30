9ab380ab63e7eb1b08a9014e70556c9c4d437ab5
DanielUnderwoodd <42712911+DanielUnderwoodd@users.noreply.github.com>
2019-07-30 10:22:30 -0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports ={
-    mongoURL:'mongodb://daniel:Apple1376@ds243317.mlab.com:43317/x-project',
+    mongoURL:'mongodb://@database_name:@database_password@ds243317.mlab.com:43317/x-project',
     secretOrkey : 'secret'
 };
